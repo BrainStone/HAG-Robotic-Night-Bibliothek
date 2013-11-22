67a7b332ba22c5cd9e4c77289211569f8a1e1c92
Markuski <xxmodplayer@gmail.com>
2013-11-22 21:49:23 +0100

    Menü!
    - Vollständiges Menü-Objekt das mit allen Objekten klar kommt

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,7 +1,7 @@
 package core;
 
-import lejos.nxt.LCD;
 import core.loader.ProgrammLoader;
+import core.template.programm.Programm;
 
 public class Main {
 	/**
@@ -11,19 +11,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(final String[] args) {
-		int anzProgramme = ProgrammLoader.Programme.size();
-		
-		int topPos = 0;
-		int programm = 0;
-		
-		int i, top;
-		
-		for(i = 0; i < 8; i++) {
-			top = i + topPos;
-			
-			if(top < anzProgramme) {
-				LCD.drawString(ProgrammLoader.Programme.get(top).name(), 0, i);
-			}
-		}
+		Menu<Programm> menu = new Menu<>(ProgrammLoader.Programme);
+		menu.auswahl().start();
 	}
 }
---
+++ src/core/Menu.java
@@ -0,0 +1,67 @@
+package core;
+
+import java.util.ArrayList;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+
+/**
+ * Menüobject : Einfaches Menü zur darstellung Verschiedener auswahlen.
+ * 
+ * @author xxMarkuski
+ * @param <TYPE>
+ * 
+ */
+public class Menu<TYPE> {
+	public ArrayList<TYPE> objects;
+
+	/**
+	 * Konstruiert ein neues Menü( {@link Object#toString()} wird als Name
+	 * genutzt)
+	 * 
+	 * @param objects
+	 *            ArrayList der Objekte im Menü
+	 */
+	public Menu(ArrayList<TYPE> objects) {
+		this.objects = objects;
+	}
+	
+	/**
+	 * Zeichnet und Überprüft das eigentliche Menü 
+	 * @return Das ausgewählte Objekt
+	 */
+	public TYPE auswahl() {
+		int size = objects.size();
+
+		TYPE selProg = null;
+		int currPos = 0;
+		int top = 0;
+
+		while (selProg == null) {
+			if (Button.RIGHT.isDown() && currPos != size) {
+				currPos++;
+			} else if (Button.LEFT.isDown() && currPos != 0) {
+				currPos--;
+			} else if (Button.ENTER.isDown()) {
+				selProg = objects.get(currPos);
+			}
+
+			if (currPos - top >= 6 && top != size) {
+				top++;
+			} else if (currPos - top <= 1 && top != 0) {
+				top--;
+			}
+
+			for (int i = top; i < top + size; i++) {
+				String selmark = " ";
+				if (i == currPos) {
+					selmark = ">";
+				}
+				//DEBUG
+				//System.out.println("for : " + i + " curr pos " + currPos+ " top " + top);
+				LCD.drawString(selmark + objects.get(i - top).toString(), 0, i - top);
+			}
+		}
+		return selProg;
+	}
+}
